ad8a924c80f7c6545688f46cc0292158608d9cc6
jrotty <bssf@qq.com>
2016-04-09 22:37:43 +0800

    Added files via upload
    删除滚动条美化部分和页码跳转功能

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * 
  * @package Themia
  * @author Jrotty
- * @version 3.5.0
+ * @version 3.5.1
  * @link http://qqdie.com
  */
 ?>
@@ -207,39 +207,18 @@ href="<?php $this->permalink() ?>"<?php };?> class="postShorten-excerpt_link lin
         </li>
         
             
-    <form name="formpage" class="pagination-number">
-<?php if ($this->options->cdl == '0'): ?>page <?php endif; ?><?php if ($this->options->cdl == '1'): ?><?php endif; ?><?php if ($this->options->cdl == '2'): ?>第<?php endif; ?>
-<input name='SkipPage' onKeyDown='if(event.keyCode==13){submitSkip.click();return false;}' 
-style='width: 15px;' type='text' value='<?php if($this->_currentPage>1) echo $this->_currentPage;  else echo 1;?>' >
-<input name='submitSkip'  id="submitSkip" type='button' onClick='GoPage("<?php $this->options->siteUrl(); ?>page/1")' style="display:none"/>
+ <li class="pagination-number">
+
+<?php if ($this->options->cdl == '0'): ?>page <?php endif; ?><?php if ($this->options->cdl == '1'): ?><?php endif; ?><?php if ($this->options->cdl == '2'): ?>第<?php endif; ?><?php if($this->_currentPage>1) echo $this->_currentPage;  else echo 1;?>
+
 
 <?php if ($this->options->cdl == '0'): ?>  of <?php endif; ?><?php if ($this->options->cdl == '1'): ?>ページ / <?php endif; ?><?php if ($this->options->cdl == '2'): ?>页/共<?php endif; ?><?php echo   ceil($this->getTotal() / $this->parameter->pageSize); ?><?php if ($this->options->cdl == '1'): ?>ページ<?php endif; ?><?php if ($this->options->cdl == '2'): ?>页<?php endif; ?>
-</form>
+</li>
     </ul>
 </div>
  
 </section>
-  <SCRIPT language=javascript>
-function GoPage(Myself)
-{
-var Lmyself=Myself.replace("1","")
-var g = /^[1-9]*[1-9][0-9]*$/;
 
-if(g.test(document.formpage.SkipPage.value)){
-if (document.formpage.SkipPage.value == 1)
-{window.location.href=Myself;} //绝对路径可自己设置
-if(document.formpage.SkipPage.value><?php echo   ceil($this->getTotal() / $this->parameter->pageSize); ?>){alert('您输入的页码不存在，请重新输入！');window.location.href=window.location.href;}
-else{
-window.location.href=Lmyself+document.formpage.SkipPage.value;
-}
-
-}else{
-alert('请输入需要跳转到的页数并且必须为正整数！');
-window.location.href=window.location.href;
-}
-
-}
-</SCRIPT>
 
 
 
