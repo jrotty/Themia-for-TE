188a2dd4ad67b547bf8d1df3eb26c4b08a876208
jrotty <bssf@qq.com>
2016-05-04 08:39:42 +0800

    Update header.php

--- header.php
+++ header.php
@@ -4,15 +4,14 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" >
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+ 
     <title><?php $this->archiveTitle(array(
 'category'=>_t('分类 %s 下的文章'),
 'search'=>_t('包含关键字 %s 的文章'),
 'tag' =>_t('标签 %s 下的文章'),
 'author'=>_t('%s 发布的文章')
-), '', ' - '); ?><?php $this->options->title(); ?><?php if($this->is('index')): ?> | <?php $this->options->description() ?>
-<?php endif; ?></title>
+), '', ' - '); ?><?php $this->options->title(); ?></title>
     
 
 <?php if (!empty($this->options->sidebarBlock) && in_array('Showcolor', $this->options->sidebarBlock)): ?>
@@ -35,4 +34,7 @@
 
     <body>
         <div id="blog">
- 
+        <!--[if lt IE 9]>
+		 <div class="browsehappy" role="dialog"><?php _e('当前网页 <strong>不支持</strong> 你正在使用的浏览器. 为了正常的访问, 请 <a href="http://browsehappy.com/">升级你的浏览器</a>'); ?>.</div>
+	    <![endif]-->
+
