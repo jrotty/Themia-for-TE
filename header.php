8ae4470c9f86f9c95b785876d7d1126e52503e8a
jrotty <bssf@qq.com>
2016-06-18 14:01:12 +0800

    Update header.php

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
    <?php $this->header("generator=&template="); ?> 
 </head>
 
-    <body>
+    <body><div id="sos">
         <div id="blog">
         <!--[if lt IE 9]>
 		 <div class="browsehappy" role="dialog"><?php _e('当前网页 <strong>不支持</strong> 你正在使用的浏览器. 为了正常的访问, 请 <a href="http://browsehappy.com/">升级你的浏览器</a>'); ?>.</div>
