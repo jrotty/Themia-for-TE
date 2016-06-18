f53fda416964f24561a565e7b8a037910d069e54
jrotty <bssf@qq.com>
2016-06-18 13:54:29 +0800

    Update post.php

--- post.php
+++ post.php
@@ -83,212 +83,68 @@ data-behavior="<?php $this->options->css(); ?>"<?php };?><?php };?>
         <div class="main-content-wrap">
 
      <?php $this->content(); ?>
-<?php if (isset($this->fields->t)): ?>
-<div class="image-gallery">
-        <div class="image-gallery-metabar">
-            <span><?php $this->fields->t(); ?></span>
-        </div>
-        <div class="image-gallery-photos image-gallery-photos--thumbnail">
-            
-                    
-                    
-        <?php if (isset($this->fields->t1)): ?>       
-             <div class="photo-box">
-                    <a class="photo-box-inner fancybox" rel="fancybox-thumb" data-fancybox-group="gallery-undefined" title="" href="<?php $this->fields->t1(); ?>" target="_blank" style="top: -168.5px;">
-                        <img class="photo" src="<?php $this->fields->t1(); ?>" itemprop="image">
-                    </a>
-                </div>
-             <?php endif; ?>   
-                    
-                    
-                    
-                
-                    
-                        <?php if (isset($this->fields->t2)): ?>    
-                
-                <div class="photo-box">
-                    <a class="photo-box-inner fancybox" rel="fancybox-thumb" data-fancybox-group="gallery-undefined" title="" href="<?php $this->fields->t2(); ?>" target="_blank" style="top: -31px;">
-                        <img class="photo" src="<?php $this->fields->t2(); ?>" itemprop="image">
-                    </a>
-                </div>
-            
-                    
-                       <?php endif; ?>   
-                    
-                
-                    
-                    <?php if (isset($this->fields->t3)): ?> 
-                
-                <div class="photo-box">
-                    <a class="photo-box-inner fancybox" rel="fancybox-thumb" data-fancybox-group="gallery-undefined" title="" href="<?php $this->fields->t3(); ?>" target="_blank" style="left: -18.9582px; top: -2.5px;">
-                        <img class="photo" src="<?php $this->fields->t3(); ?>" itemprop="image" style="height: 328px; width: 584.916px;">
-                    </a>
-                </div>
-            <?php endif; ?>   
-                    
-                    
-                    
-                
-                    
-                           <?php if (isset($this->fields->t4)): ?> 
-                
-                <div class="photo-box">
-                    <a class="photo-box-inner fancybox" rel="fancybox-thumb" data-fancybox-group="gallery-undefined" title="" href="<?php $this->fields->t4(); ?>" target="_blank" style="top: -2px; left: -0.333333px;">
-                        <img class="photo" src="<?php $this->fields->t4(); ?>" itemprop="image" style="height: 328px; width: 546.667px;">
-                    </a>
-                </div>
-                 <?php endif; ?>   
-                    
-                    
-                    
-                
-                    
-                    
-                       <?php if (isset($this->fields->t5)): ?> 
-                <div class="photo-box">
-                    <a class="photo-box-inner fancybox" rel="fancybox-thumb" data-fancybox-group="gallery-undefined" title="" href="<?php $this->fields->t5(); ?>" target="_blank" style="top: -2px; left: -0.333333px;">
-                        <img class="photo" src="<?php $this->fields->t5(); ?>" itemprop="image" style="height: 328px; width: 546.667px;">
-                    </a>
-                </div>
-            
-                    
-                    
-                         <?php endif; ?>   
-                
-                    
-                           <?php if (isset($this->fields->t6)): ?> 
-                
-                <div class="photo-box">
-                    <a class="photo-box-inner fancybox" rel="fancybox-thumb" data-fancybox-group="gallery-undefined" title="" href="<?php $this->fields->t6(); ?>" target="_blank" style="top: -2px; left: -0.333333px;">
-                        <img class="photo" src="<?php $this->fields->t6(); ?>" itemprop="image" style="height: 328px; width: 546.667px;">
-                    </a>
-                </div>     <?php endif; ?>   
-            
-                    
-                    
-                    
-                
-                    
-                    
-                       <?php if (isset($this->fields->t7)): ?> 
-                <div class="photo-box">
-                    <a class="photo-box-inner fancybox" rel="fancybox-thumb" data-fancybox-group="gallery-undefined" title="" href="<?php $this->fields->t7(); ?>" target="_blank" style="top: -2px; left: -0.333333px;">
-                        <img class="photo" src="<?php $this->fields->t7(); ?>" itemprop="image" style="height: 328px; width: 546.667px;">
-                    </a>
-                </div>
-            
-                         <?php endif; ?>   
-                    
-                    
-                
-                    
-                    
-                       <?php if (isset($this->fields->t8)): ?> 
-                <div class="photo-box">
-                    <a class="photo-box-inner fancybox" rel="fancybox-thumb" data-fancybox-group="gallery-undefined" title="" href="<?php $this->fields->t8(); ?>" target="_blank" style="top: -2px; left: -0.333333px;">
-                        <img class="photo" src="<?php $this->fields->t8(); ?>" itemprop="image" style="height: 328px; width: 546.667px;">
-                    </a>
-                </div>     <?php endif; ?>   
-            
-                    
-                    
-                    
-
-       
-                    
-                       <?php if (isset($this->fields->t9)): ?> 
-                <div class="photo-box">
-                    <a class="photo-box-inner fancybox" rel="fancybox-thumb" data-fancybox-group="gallery-undefined" title="" href="<?php $this->fields->t9(); ?>" target="_blank" style="top: -2px; left: -0.333333px;">
-                        <img class="photo" src="<?php $this->fields->t9(); ?>" itemprop="image" style="height: 328px; width: 546.667px;">
-                    </a>
-                </div>     <?php endif; ?>   
-            
-                    
-                    
-                    
-                
-                    
-                    
-                       <?php if (isset($this->fields->t10)): ?> 
-                <div class="photo-box">
-                    <a class="photo-box-inner fancybox" rel="fancybox-thumb" data-fancybox-group="gallery-undefined" title="" href="<?php $this->fields->t10(); ?>" target="_blank" style="top: -2px; left: -0.333333px;">
-                        <img class="photo" src="<?php $this->fields->t10(); ?>" itemprop="image" style="height: 328px; width: 546.667px;">
-                    </a>
-                </div>     <?php endif; ?>   
-            
-        </div>
-    </div><?php endif; ?> <?php if (isset($this->fields->t0)): ?> <p><?php $this->fields->t0(); ?></p><?php endif; ?> 
-    
- <div style="float:right">
-<?php if (!empty($this->options->sidebarBlock) && in_array('bjq', $this->options->sidebarBlock)): ?>
-最后由 <?php $this->author() ?> 编辑于<?php echo gmdate('Y年m月d日 H:i', $this->modified + Typecho_Widget::widget('Widget_Options')->timezone); ?><?php endif; ?> </div> 
-        </div>
+ </div>
     </div>
     <div class="post-footer main-content-wrap">
         
-            <div class="post-footer-tags">
+        <?php if($this->is('post')): ?>
+
+ <?php if (!empty($this->options->sidebarBlock) && in_array('kp', $this->options->sidebarBlock)): ?>
+
+
+<div style=" text-align: center;">  
+
+    <div id="QR" style="display: none;">
+      
+        <div id="wechat" style="display: inline-block;    padding-right: 7px;">
+         <img id="wechat_qr" src="https://qqdie.com/usr/uploads/2016/05/1196403231.png" alt="jrotty WeChat Pay" >
+          <p>微信打赏</p>
+        </div>
+        <div id="alipay" style="display: inline-block;    padding-left: 7px;"><img id="alipay_qr" <?php if ($this->options->zfbUrl){ ?>src="<?php $this->options->zfbUrl();?>"<?php }else{ ?>src="https://qqdie.com/usr/uploads/2016/05/3220161479.png"<?php };?> alt="jrotty Alipay">
+          <p>支付宝打赏</p>
+        </div>
+    
+    </div>
+
+
+  <div id="ew" style="display: none;">
+      
+        
+    <div id="erweima" style="display: inline-block">
+
+<img id="erwei_qr" src="https://qqdie.com/er/qrcode.php?&url=<?php $this->permalink() ?>&p=4.8685"/>
+ <p>文章二维码</p>
+ </div>
+    </div>
+
+
+
+
+  <a id="rewardButton" disable="enable" onclick="var qr = document.getElementById('ew');var ds = document.getElementById('QR');if (qr.style.display === 'none') {qr.style.display='block';} else {qr.style.display='none'}{ds.style.display='none'}" class="tag tag--primary tag--small t-link">
+   文章二维码
+    </a>
+  <a id="rewardButton" disable="enable" onclick="var qr = document.getElementById('QR'); var ds = document.getElementById('ew');if (qr.style.display === 'none') {qr.style.display='block';} else {qr.style.display='none'} {ds.style.display='none'}" class="tag tag--primary tag--small t-link">
+   赏
+    </a>
+
+  </div>
+
+
+<?php endif; ?>
+
+        
+        
+            <div class="post-footer-tags"><?php if (!empty($this->options->sidebarBlock) && in_array('bjq', $this->options->sidebarBlock)): ?>
+<div style="float:right">最后由<a href="<?php $this->author->permalink(); ?>"><?php $this->author(); ?></a>编辑于<?php echo gmdate('Y:m:d', $this->modified + Typecho_Widget::widget('Widget_Options')->timezone); ?>  <xa title="点我点我" id="ymzz" ></xa>
+
+</div> <?php endif; ?> 
                 <span class="text-color-light text-small"><?php if ($this->options->cdl == '0'): ?>TAGGED IN<?php endif; ?><?php if ($this->options->cdl == '1'): ?>文章标签：<?php endif; ?></span><br/>
 
 
 <nav class="tag tag--primary tag--small t-link" style=" display: inline-block;">  
 <?php if ($this->options->cdl == '0'): ?><?php $this->tags('</nav><nav class="tag tag--primary tag--small t-link" style=" display: inline-block;">', true, ' <sx>none</sx>'); ?><?php endif; ?><?php if ($this->options->cdl == '1'): ?><?php $this->tags('</nav><nav class="tag tag--primary tag--small t-link" style=" display: inline-block;">', true, ' <sx>无标签</sx>'); ?><?php endif; ?>
 
-</nav>  <?php if (!empty($this->options->sidebarBlock) && in_array('kp', $this->options->sidebarBlock)): ?>
-
-
-  <div id="social">
-<div class="social-main">
-<span class="like">
-<a data-dialog="#popup-erwei" data-effect="effect-newspaper" title="跨屏浏览">&nbsp;跨屏浏览</a>
-</span>
-<span class="shang-p">
-<a data-dialog="#popup-shang" data-effect="effect-slide-top" title="赞助本站">赏</a>
-</span>
-<span class="share-s">
-<a data-dialog="#popup-share" data-effect="effect-newspaper" title="版权声明">&nbsp;版权声明</a>
-</span>
-<div class="clear"></div>
-</div>
-</div>
-<div class="popup effect-fade-scale" id="popup-erwei">
-<div class="popup-content"><center>
-<h3><i class="sidebar-button-icon fa fa-mobile-phone"></i>手机扫描下方二维码</h3>
-<div class="bdsharebuttonbox">
-
-
-
-<img src="//pan.baidu.com/share/qrcode?w=200&h=200&url=<?php $this->permalink() ?>" alt="<?php $this->title() ?>"/>
-
-</div></center></div>
-</div>
-
-<div class="popup effect-fade-scale" id="popup-shang">
-<div class="popup-content"><center><h3>
-支付宝转账赞助本站</h3>
-<div class="bdsharebuttonbox">
-<img width="200" height="200" <?php if ($this->options->zfbUrl){ ?>src="<?php $this->options->zfbUrl();?>"<?php }else{ ?>src="https://mobilecodec.alipay.com/show.htm?code=apx04121sgsu8ym0ho9wt59&picSize=L"<?php };?> title="支付宝转账赞助" />
-<!--<p><img width="280" height="280" src="" title="微信转账赞助" /></p>--></div></center>
-
-</div>
-</div>
-<div class="popup effect-fade-scale" id="popup-share">
-<div class="popup-content"><div class="bdsharebuttonbox">
-著作权归作者所有。<br>
-商业转载请联系作者获得授权，非商业转载请注明出处。<br>
-作者：<?php $this->author() ?><br>
-链接：<?php $this->permalink() ?><br>
-</div></div>
-</div>
-
-<?php endif; ?> 
-
-
-
-
-
-
-
-            </div>
+</nav>  </div><?php endif;?>
         
         <div class="post-actions-wrap">
     <nav>
@@ -351,8 +207,4 @@ data-behavior="<?php $this->options->css(); ?>"<?php };?><?php };?>
         
     </div>
 </article>
-
-
-                    
-
 	<?php $this->need('footer.php'); ?>
