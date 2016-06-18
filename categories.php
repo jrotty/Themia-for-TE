15ef56377c5fad6c0b5a7f4250685a379b0db2d2
jrotty <bssf@qq.com>
2016-06-18 13:30:18 +0800

    Update categories.php

--- categories.php
+++ categories.php
@@ -6,65 +6,82 @@
  * 
  */
  $this->need('header.php'); 
-?>  <header id="header" 
+?> 
 
-data-behavior="<?php $this->options->css(); ?>">
-
-
-  <i id="btn-open-sidebar" class="fa fa-lg fa-bars"></i>
-    <h1 class="header-title">
-        <a class="header-title-link" href="<?php $this->options->siteUrl(); ?>" ><?php $this->options->title(); ?></a>
-    </h1>
-    
-        
-            <a  class="header-right-icon st-search-show-outputs"
-                href="#about">
-        
-                <i class="fa fa-lg fa-search"></i>
-            </a>
-    
-</header>
-
- <nav id="sidebar"
-
-data-behavior="<?php $this->options->css(); ?>">
-
-<?php $this->need('sidebar.php'); ?>
   
             <div id="main" data-behavior="<?php $this->options->css(); ?>"
                  class="
                         hasCoverMetaIn
                         ">
                 <div id="categories-archives" class="main-content-wrap">
- 按分类归档
-    <h4 class="archive-result text-color-base text-xlarge"></h4>
-    <section>
+
+         <?php Typecho_Widget::widget('Widget_Stat')->to($stat); ?>
+
+<style>
+a, a:active, a:hover, a:visited {
+    text-decoration: none;
+}
+.category--primary, a.category--primaryb {
+    color: #212427!important;
+    border: 1px solid #64b5f6;
+}
+</style>
+    <h4 class="archive-result text-color-base text-xlarge"> </h4>
+   <section>
 
 
-<?php $this->widget('Widget_Metas_Category_List')->to($category); ?>
-    <?php while ($category->next()): ?>
+                   <?php $this->widget('Widget_Metas_Category_List')->to($categorys); ?>
+                    <?php while($categorys->next()): ?>
+                        <?php if ($categorys->levels === 0): ?>
+                            <?php $children = $categorys->getAllChildren($categorys->mid); ?>
+                            <?php if (empty($children)) { ?>
+                             
+                                    <a href="#posts-list-<?php $categorys->name(); ?>" class="category category--small category--primary"  data-category="<?php $categorys->name(); ?>" ><?php $categorys->name(); ?>(<?php $categorys->count(); ?>)
+                                        </a>
+                                
+                         
+                            <?php } ?>
+                        <?php endif; ?>
+                    <?php endwhile; ?>
 
-<a class="category category--small category--primary" href="#posts-list-<?php $category->name(); ?>" data-category="<?php $category->count(); ?>篇文章"><?php $category->name(); ?>(<?php $category->count(); ?>)</a>
+ <?php $this->widget('Widget_Metas_Category_List')->to($categorys); ?>
+                    <?php while($categorys->next()): ?>
+                        <?php if ($categorys->levels === 0): ?>
+                            <?php $children = $categorys->getAllChildren($categorys->mid); ?>
+                            <?php if (empty($children)) { ?>
+                        <?php } else { ?>
+                            <br>
+ <a class="category category--small category--primaryb"  data-category="<?php $categorys->name(); ?>"  onclick="var fu = document.getElementById('<?php $categorys->name(); ?>'); if (fu.style.display === 'none') {fu.style.display='inline';} else {fu.style.display='none'}" ><?php $categorys->name(); ?>&nbsp;<i class="fa fa-chevron-circle-right"></i>
+                                        </a>
+                                 <div id="<?php $categorys->name(); ?>" style="display: none;">
+                                        <?php foreach ($children as $mid) { ?>
+                                            <?php $child = $categorys->getCategory($mid); ?>
+                                           
 
- <?php endwhile; ?>
+<a href="#posts-list-<?php echo $child['name']; ?> " class="category category--small category--primary"  data-category="<?php echo $child['name']; ?>" ><?php echo $child['name']; ?>(<?php echo $child['count']; ?>)</a>
+                                         
+                                        <?php } ?>
+                                   </div>
+                            <?php } ?>
+                        <?php endif; ?>
+                    <?php endwhile; ?>
 
         
     </section>
-    <section class="boxes" id="disqus_thread">
-
-        
+    <section class="boxes"  id="disqus_thread">
+    
+   
             <?php $this->widget('Widget_Metas_Category_List')->to($categories); ?>
 <?php while ($categories->next()): ?>
+<?php if(count($categories->children) === 0): ?>
 
+<?php $this->widget('Widget_Archive@category-' . $categories->mid, 'pageSize=10000&type=category', 'mid=' . $categories->mid)->to($posts); ?>  
+<div id="posts-list-<?php $categories->name(); ?>" class="archive box" data-category="<?php $categories->name(); ?>">
 
-<?php $this->widget('Widget_Archive@category-' . $categories->mid, 'pageSize=10000&type=category', 'mid=' . $categories->mid)->to($posts); ?>  <div id="posts-list-Themia" class="archive box" data-category="themia">
-
-            
+             
                     <h4 class="archive-title">
                         <a href="<?php $categories->permalink(); ?>" class="link-unstyled" id="posts-list-<?php $categories->name(); ?>"><?php $categories->name(); ?></a>
                     </h4>
-
-
                     <ul class="archive-posts">
 <?php while ($posts->next()): ?> 
                             <li class="archive-post">
@@ -78,13 +95,27 @@ data-behavior="<?php $this->options->css(); ?>">
                         
                     </ul>
                 </div>
+<?php else: ?>
+
+
+
+
+
+
+
+
+
+
+<?php endif; ?>
          <?php endwhile; ?>
-        <div id="duoshuo_thread">
+        
+<div id="duoshuo_thread">
     <?php $this->need('comments.php'); ?>
 
 </div>
-    </section>
-</div>
 
-  
- <?php $this->need('footer.php');?>
+
+
+ 
+    </section>
+</div> <?php $this->need('footer.php');?>
