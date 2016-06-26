e259b7d26a4902bab0bcd099d2b0cb63b0e20577
jrotty <bssf@qq.com>
2016-06-26 12:55:25 +0800

    评论列表头像加入爬墙

--- functions.php
+++ functions.php
@@ -320,4 +320,55 @@ $link = '<a class="post-action-btn btn btn--disabled" target="_blank">
 echo $link;
 }
 }
+//自定义评论列表区域
+function threadedComments($comments, $options) {
+    $commentClass = '';
+    if ($comments->authorId) {
+        if ($comments->authorId == $comments->ownerId) {
+            $commentClass .= ' comment-by-author';
+        } else {
+            $commentClass .= ' comment-by-user';
+        }
+    }
+
+    $commentLevelClass = $comments->levels > 0 ? ' comment-child' : ' comment-parent';
+?>
+
+<li id="li-<?php $comments->theId(); ?>" class="comment-body<?php 
+if ($comments->levels > 0) {
+    echo ' comment-child';
+    $comments->levelsAlt(' comment-level-odd', ' comment-level-even');
+} else {
+    echo ' comment-parent';
+}
+$comments->alt(' comment-odd', ' comment-even');
+echo $commentClass;
+?>">
+    <div id="<?php $comments->theId(); ?>">
+        <div class="comment-author">
+            <?php
+            //头像CDN by Rich
+            $host = 'https://secure.gravatar.com'; //自定义头像CDN服务器
+            $url = '/avatar/'; //自定义头像目录,一般保持默认即可
+            $size = '32'; //自定义头像大小
+            $rating = Helper::options()->commentsAvatarRating;
+            $hash = md5(strtolower($comments->mail));
+            $avatar = $host . $url . $hash . '?s=' . $size . '&r=' . $rating . '&d=';
+            ?>
+            <img class="avatar" src="<?php echo $avatar ?>" alt="<?php echo $comments->author; ?>" width="<?php echo $size ?>" height="<?php echo $size ?>" />
+            <cite class="fn"><?php $comments->author(); ?></cite>
+        </div>
+        <div class="comment-meta">
+            <a href="<?php $comments->permalink(); ?>"><?php $comments->date('Y-m-d H:i'); ?></a>
+            <span class="comment-reply"><?php $comments->reply(); ?></span>
+        </div>
+        <?php $comments->content(); ?>
+    </div>
+<?php if ($comments->children) { ?>
+    <div class="comment-children">
+        <?php $comments->threadedComments($options); ?>
+    </div>
+<?php } ?>
+</li>
+<? }
 ?>
