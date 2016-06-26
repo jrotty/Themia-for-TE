3d96f0f574b9c838d3f29d343550ed0ca9a66589
jrotty <bssf@qq.com>
2016-06-26 13:06:20 +0800

    Update post.php

--- post.php
+++ post.php
@@ -165,20 +165,17 @@ data-behavior="<?php $this->options->css(); ?>"<?php };?><?php };?>
             <a class="post-action-btn btn btn--default btn-open-shareoptions"  href="#btn-open-shareoptions">
                 <i class="fa fa-share-alt"></i>
             </a>
+        </li> <li class="post-action hide-xs">
+<a class="post-action-btn btn btn--default" target="_blank" data-tooltip="分享至Google"  href="https://plus.google.com/share?url=<?php $this->permalink() ?>">
+                <i class="fa fa-google-plus"></i>
+            </a>
+
         </li>
    <li class="post-action hide-xs">
            <a class="post-action-btn btn btn--default tooltip--top" target="new" data-tooltip="分享至QQ空间" href="http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=<?php $this->permalink() ?>&title=<?php $this->title() ?>&site=<?php $this->options->title(); ?>/&pics=<?php showThumbnail($this); ?>">
                 <i class="fa fa-qq"></i>
             </a>
         </li>
-        <li class="post-action hide-xs">
-            <a class="post-action-btn btn btn--default tooltip--top" target="new" data-tooltip="分享至人人网" href="http://widget.renren.com/dialog/share?resourceUrl=<?php $this->permalink() ?>/&srcUrl=<?php $this->permalink() ?>&pic=<?php showThumbnail($this); ?>&title=<?php $this->title() ?>">
-                <i class="fa fa-renren"></i>
-            </a>
-        </li>
-       
-     
-
          <li class="post-action hide-xs">
             <a class="post-action-btn btn btn--default" target="new" data-tooltip="分享至新浪微博" href="http://service.weibo.com/share/share.php?url=<?php $this->permalink() ?>/&appkey=<?php $this->options->title(); ?>/&title=<?php $this->title() ?>&pic=<?php showThumbnail($this); ?>">
                 <i class="fa fa-weibo"></i>
