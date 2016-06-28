602a587c8e397c1fb8dbcfa73207dbc58a65fc07
jrotty <bssf@qq.com>
2016-06-28 20:39:28 +0800

    Update header.php

--- header.php
+++ header.php
@@ -79,7 +79,7 @@ content="<?php showThumbnail($this); ?>"<?php else: ?>
 		 <div class="browsehappy" role="dialog"><?php _e('当前网页 <strong>不支持</strong> 你正在使用的浏览器. 为了正常的访问, 请 <a href="http://browsehappy.com/">升级你的浏览器</a>'); ?>.</div>
 	    <![endif]-->
 
-<?php if($this->is('post')): ?> 
+<?php if($this->is('post') || $this->is('page')): ?> 
 
 
 
