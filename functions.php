d1538843f96596fa595655846b2d7e5e19686de9
jrotty <bssf@qq.com>
2016-07-01 17:28:48 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -60,10 +60,10 @@ array(
 '4' => _t('4.在所有屏幕显示抽屉SideBar（窄）'),
 ),
 '1',
-_t('sidebar风格'), _t('这里的风格指的是网站整体风格，文章风格可通过自定义字段字段名ys和字段值来控制，实现对单独的一篇文章的风格重定义（字段值就是上边对应的数字，如风格3，就在文章字段名填入ys，字段值填入3即可），文章封面效果也是用自定义字段，字段名fm，字段值就是封面的图片地址，启动封面效果的文章风格会变更为3（优先级高）√')); 
+_t('风格选择'), _t('这里的风格指的是网站整体风格，文章风格可通过自定义字段字段名ys和字段值来控制，实现对单独的一篇文章的风格重定义（字段值就是上边对应的数字，如风格3，就在文章字段名填入ys，字段值填入3即可），文章封面效果也是用自定义字段，字段名fm，字段值就是封面的图片地址，启动封面效果的文章风格会变更为3（优先级高）√')); 
 $form->addInput($css->multiMode());
-//居中风格
-  $tf = new Typecho_Widget_Helper_Form_Element_Radio('tf', 
+
+ $tf = new Typecho_Widget_Helper_Form_Element_Radio('tf', 
     array(
         'open' => _t('启用居中风格，使用默认方式截取缩略内容'),  
         'more' => _t('启用居中风格，并强制使用more语法截取缩略内容'), 
@@ -72,11 +72,11 @@ $form->addInput($css->multiMode());
     'close', 
     _t('文章标题居中风格'), _t('默认不启用，启用后文章缩略图会关闭显示，并且文章标题，分类等信息居中'));
     $form->addInput($tf);
+
     //工具开关
     $sidebarBlock = new Typecho_Widget_Helper_Form_Element_Checkbox('sidebarBlock', 
     array('kp' => _t('文章中加入跨屏浏览二维码，支付宝打赏，及文章版权声明')
-,
-'mulu' => _t('开启文章目录树功能'),
+,'mulu' => _t('开启文章目录树功能'),
 	'bjq' => _t('文章内容增强，加入最后编辑时间'),
 	'Showfull' => _t('手机浏览器强制全屏，仅限于uc和QQ浏览器')
 ,'Showcolor' => _t('手机谷歌浏览器状态栏颜色渲染为白色'),
@@ -330,55 +330,4 @@ $link = '<a class="post-action-btn btn btn--disabled" target="_blank">
 echo $link;
 }
 }
-//自定义评论列表区域
-function threadedComments($comments, $options) {
-    $commentClass = '';
-    if ($comments->authorId) {
-        if ($comments->authorId == $comments->ownerId) {
-            $commentClass .= ' comment-by-author';
-        } else {
-            $commentClass .= ' comment-by-user';
-        }
-    }
-
-    $commentLevelClass = $comments->levels > 0 ? ' comment-child' : ' comment-parent';
-?>
-
-<li id="li-<?php $comments->theId(); ?>" class="comment-body<?php 
-if ($comments->levels > 0) {
-    echo ' comment-child';
-    $comments->levelsAlt(' comment-level-odd', ' comment-level-even');
-} else {
-    echo ' comment-parent';
-}
-$comments->alt(' comment-odd', ' comment-even');
-echo $commentClass;
-?>">
-    <div id="<?php $comments->theId(); ?>">
-        <div class="comment-author">
-            <?php
-            //头像CDN by Rich
-            $host = 'https://secure.gravatar.com'; //自定义头像CDN服务器
-            $url = '/avatar/'; //自定义头像目录,一般保持默认即可
-            $size = '32'; //自定义头像大小
-            $rating = Helper::options()->commentsAvatarRating;
-            $hash = md5(strtolower($comments->mail));
-            $avatar = $host . $url . $hash . '?s=' . $size . '&r=' . $rating . '&d=';
-            ?>
-            <img class="avatar" src="<?php echo $avatar ?>" alt="<?php echo $comments->author; ?>" width="<?php echo $size ?>" height="<?php echo $size ?>" />
-            <cite class="fn"><?php $comments->author(); ?></cite>
-        </div>
-        <div class="comment-meta">
-            <a href="<?php $comments->permalink(); ?>"><?php $comments->date('Y-m-d H:i'); ?></a>
-            <span class="comment-reply"><?php $comments->reply(); ?></span>
-        </div>
-        <?php $comments->content(); ?>
-    </div>
-<?php if ($comments->children) { ?>
-    <div class="comment-children">
-        <?php $comments->threadedComments($options); ?>
-    </div>
-<?php } ?>
-</li>
-<? }
 ?>
