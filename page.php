8a05c07dd1f402b71074fa18de54bd439223f643
jrotty <bssf@qq.com>
2016-07-10 15:38:40 +0800

    Update page.php

--- page.php
+++ page.php
@@ -1,5 +1,20 @@
 <?php $this->need('post.php'); ?>
-
+<?php if ($this->fields->li): ?>
+<style>
+li.fen{
+    display: block;
+}
+@media only screen and (min-width:1024px){
+li.fen{
+  float: left;
+    width: 364px;
+   white-space:nowrap; 
+    text-overflow: ellipsis;
+    overflow: hidden;
+}
+}
+</style>
+<?php endif; ?>
 <SCRIPT language=javascript>
 $('#yl ul li').each(function(){
   $(this).addClass('fen')
