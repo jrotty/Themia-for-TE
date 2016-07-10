fb12de00da9b3eeac7c4cccd9ac1f3f7f0de7299
jrotty <bssf@qq.com>
2016-07-10 15:57:03 +0800

    Update header.php

--- header.php
+++ header.php
@@ -4,31 +4,19 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" >
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
- 
+   <meta meta http-equiv="X-UA-Compatible" content="IE=edge">
+<?php if (!empty($this->options->sidebarBlock) && in_array('Showcolor', $this->options->sidebarBlock)): ?><meta name="theme-color" content="#fff"><?php endif; ?> 
+<?php if (!empty($this->options->sidebarBlock) && in_array('Showfull', $this->options->sidebarBlock)): ?><meta name="full-screen" content="yes">  
+<meta name="x5-fullscreen" content="true"> <?php endif; ?> <meta http-equiv="Page-Exit" ;="" content="blendTrans(Duration=1.0)">
     <title><?php $this->archiveTitle(array(
 'category'=>_t('分类 %s 下的文章'),
 'search'=>_t('包含关键字 %s 的文章'),
 'tag' =>_t('标签 %s 下的文章'),
-'author'=>_t('%s 发布的文章')
-), '', ' - '); ?><?php $this->options->title(); ?><?php if($this->is('index')): ?> | <?php $this->options->description() ?>
+'author'=>_t('%s 的主页')
+), '', ' - '); ?><?php $this->options->title(); ?><?php if($this->is('index')): ?> | <?php $this->options->description() ?>-第<?php if($this->_currentPage>1) echo $this->_currentPage;  else echo 1;?>页
 <?php endif; ?></title>
-    
 
-<?php if (!empty($this->options->sidebarBlock) && in_array('Showcolor', $this->options->sidebarBlock)): ?>
-<meta name="theme-color" content="#fff">
-
-<?php endif; ?> 
-<?php if (!empty($this->options->sidebarBlock) && in_array('Showfull', $this->options->sidebarBlock)): ?>
-
-<meta name="full-screen" content="yes">  
-<meta name="x5-fullscreen" content="true"> <?php endif; ?> 
-        <link rel="icon" id="web-icon" href="/favicon.ico">
-        <link rel="apple-touch-icon" href="<?php $this->options->themeUrl('favicon.png'); ?>"/>
-<link rel="alternative" type="application/atom+xml" title="RSS" href="<?php $this->options->feedUrl(); ?>">
-
- 
-  <meta property="og:image" <?php if($this->is('post')||$this->is('page')): ?>
+ <meta property="og:image" <?php if($this->is('post')||$this->is('page')): ?>
 content="<?php showThumbnail($this); ?>"<?php else: ?>
 <?php if ($this->options->logoUrl){ ?>content="<?php $this->options->logoUrl();?>"<?php }else{ ?>content="<?php $this->options->themeUrl('image/avatar.jpg'); ?>"<?php };?><?php endif; ?>>
 <meta property="og:title" content="<?php $this->archiveTitle(array(
@@ -54,39 +42,48 @@ content="<?php showThumbnail($this); ?>"<?php else: ?>
 <meta itemprop="image" <?php if($this->is('post')||$this->is('page')): ?>
 content="<?php showThumbnail($this); ?>"<?php else: ?>
 <?php if ($this->options->logoUrl){ ?>content="<?php $this->options->logoUrl();?>"<?php }else{ ?>content="<?php $this->options->themeUrl('image/avatar.jpg'); ?>"<?php };?><?php endif; ?>>
+
+ <!--STYLES-->
+<link rel="stylesheet" href="<?php $this->options->themeUrl('fontawesome.css'); ?>" type="text/css">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('style.min.css'); ?>" type="text/css">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('duoshuo.css'); ?>" type="text/css">
+
+
+<link  id="web-icon" rel="shortcut icon" href="/favicon.ico"/>
+<link rel="apple-touch-icon" href="<?php $this->options->themeUrl('favicon.png'); ?>"/>
+<link rel="alternative" type="application/atom+xml" title="RSS" href="<?php $this->options->feedUrl(); ?>">    
+<!--STYLES END--><meta property="wb:webmaster" content="8a2a0fa4ce931220" />
  
- <!--STYLES--> <link rel="stylesheet" href="<?php $this->options->themeUrl('fontawesome.css'); ?>" type="text/css">
-    <link rel="stylesheet" href="<?php $this->options->themeUrl('style.min.css'); ?>" type="text/css">
-    <?php if($this->options->tf == 'open'||$this->options->tf == 'more'):?>
+<?php if($this->options->tf == 'open'||$this->options->tf == 'more'):?>
 <style>.postShorten-header {text-align: center;}</style>
 <?php endif; ?>
 <?php if($this->options->page_suo == 'display'):?>
 <style> p{text-indent: 2em;}p iframe{margin-left:-2em;} </style>
 <?php endif; ?>
 <?php if($this->options->page_suo == 'display1'):?><?php if($this->is('post')): ?>
-<style> p{text-indent: 2em;}p iframe{margin-left:-2em;} </style>
+<style>.main-content-wrap p{text-indent: 2em;} .main-content-wrap p iframe{margin-left:-2em;} </style>
 <?php endif; ?><?php endif; ?>
 <?php if($this->options->page_suo == 'display2'):?><?php if($this->is('page')): ?>
-<style> p{text-indent: 2em;}p iframe{margin-left:-2em;} </style>
+<style>.main-content-wrap p{text-indent: 2em;} .main-content-wrap p iframe{margin-left:-2em;} </style>
 <?php endif; ?><?php endif; ?>
 <?php if($this->options->page_suo == 'display3'):?><?php if($this->is('index')): ?>
 <style> p{text-indent: 2em;}p iframe{margin-left:-2em;} </style>
 <?php endif; ?><?php endif; ?>
-    <!--STYLES END-->
-   <?php $this->header("generator=&template="); ?> 
+<?php $this->header("generator=&template="); ?>
 </head>
-
-    <body><div id="sos">
-        <div id="blog">
-        <!--[if lt IE 9]>
+<body>
+<div id="sos">
+<div id="blog">  <!--[if lt IE 9]>
 		 <div class="browsehappy" role="dialog"><?php _e('当前网页 <strong>不支持</strong> 你正在使用的浏览器. 为了正常的访问, 请 <a href="http://browsehappy.com/">升级你的浏览器</a>'); ?>.</div>
 	    <![endif]-->
 
-<?php if($this->is('post') || $this->is('page')): ?> 
-
-
-
-<header id="header" <?php if ($this->fields->fm){ ?>data-behavior="3"<?php }else{ ?>
+<?php if($this->is('post')||$this->is('page')): ?> 
+<?php if (isset($this->fields->hb)): ?>
+<style type="text/css">
+html{filter:progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);-webkit-filter: grayscale(100%);}
+</style>
+ <?php endif; ?>
+<header id="header" <?php if ($this->fields->fm){ ?>data-behavior="3" style=" opacity: 0.8;"<?php }else{ ?>
  <?php if ($this->fields->ys){ ?>data-behavior="<?php $this->fields->ys(); ?>"<?php }else{ ?>
 
 data-behavior="<?php $this->options->css(); ?>"<?php };?><?php };?>>
@@ -99,7 +96,7 @@ data-behavior="<?php $this->options->css(); ?>"<?php };?><?php };?>>
     
         
             <a  class="header-right-icon st-search-show-outputs"
-                id="sou">
+                id="sou" target="_blank">
         
                 <i class="fa fa-lg fa-search"></i>
             </a>    
@@ -113,7 +110,7 @@ data-behavior="<?php $this->options->css(); ?>"<?php };?><?php };?>>
     
         
             <a  class="header-right-icon st-search-show-outputs"
-                id="sou">
+                id="sou" target="_blank">
         
                 <i class="fa fa-lg fa-search"></i>
             </a>
@@ -121,5 +118,6 @@ data-behavior="<?php $this->options->css(); ?>"<?php };?><?php };?>>
 </header>
 
 <?php endif; ?>
+
 
 <?php $this->need('sidebar.php'); ?>
