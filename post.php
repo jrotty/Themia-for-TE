0437a2a084188bd01fee9b7b609ce00caffd3069
jrotty <bssf@qq.com>
2017-09-21 10:36:41 +0800

    文字二维码api换掉百度
    文字二维码api换掉百度，使用某个人api，支持使用logo

--- post.php
+++ post.php
@@ -109,7 +109,7 @@ text-center<?php else: ?>text-left<?php endif; ?>">
         
     <div id="erweima" style="display: inline-block">
 
-<img id="erwei_qr" src="https://pan.baidu.com/share/qrcode?w=143&h=143&url=<?php $this->permalink() ?>"/>
+<img id="erwei_qr" src="https://api.imjad.cn/qrcode?text=<?php $this->permalink() ?>&logo=https%3A%2F%2Fapi.imjad.cn%2Fqrcode%2Flogo.png&size=200&level=M&bgcolor=%23ffffff&fgcolor=%23000000"/>
  <p style=" text-indent: 0em; margin-left:-0em;">文章二维码</p>
  </div>
     </div>
